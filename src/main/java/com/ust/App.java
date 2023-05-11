5194fd1ccfa6e67b14ca400c9a58de1c9f58baa0
root <root@ip-172-31-85-68.ec2.internal>
2023-05-11 10:04:47 +0000

    first_commit

---
+++ src/main/java/com/ust/App.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
